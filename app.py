9b8ee3593b3023a2e6225216aefa2c3169435ed7
winter0019 <121707096+winter0019@users.noreply.github.com>
2025-09-02 21:37:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ import requests
 import pytesseract
 from PIL import Image
 import fitz # PyMuPDF for better PDF text extraction
+import firebase_admin
 
 
 
@@ -1030,4 +1031,5 @@ def fetch_user_history(email, limit=10):
 
 if __name__ == "__main__":
     app.run(debug=True, port=int(os.getenv('PORT', 5000)))
+
 
